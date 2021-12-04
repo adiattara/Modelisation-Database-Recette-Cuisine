1ce8ca9b759394967853cb461ab2498402d13477
= <=>
2021-12-04 19:37:14 +0100

    modif

--- requetes/sql/01.sql
+++ requetes/sql/01.sql
@@ -1,23 +1,51 @@
-select "idRecette", "nomRecette"
-from "Recettes" r
-where ("calories" / "nombreDePersonnes") < 200
-  and r."idRecette" not in (
-    select "idRecette"
-    from "Recettes"
-    where r."idRecette" in (
-        select r."idRecette"
-        from "Ingredients_Recettes"
-        where "idIngredient" in (
-            select iir."idIngredient"
-            from "Regimes" r,
-                 "Ingredients_Incompatibles_Regimes" iir
-            where r."idRecette" = iir."idRegime"
-              and r."nomRegime" = 'Sans gluten'
-        )
-    )
-      and r."idRecette" in (
-        select distinct r."idRecette"
-        from "Planning_Recettes_Utilisateurs" pu
-        where r."idRecette" = pu."idRecette"
-    )
-);
+select  "idRecette" ,"nomRecette" 
+from "Recettes"  r
+where(r."calories"/r."nombreDePersonnes") < 200
+and r."idRecette" 
+in 
+(
+    select ir."idRecette" 
+    from "Ingredients_Recettes" ir
+    where ir."idIngredient" 
+    in (
+        select iir."idIngredient" 
+        from "Ingredients_Incompatibles_Regimes" iir
+        where iir."idRegime" 
+        in (
+            select rg."idRegime" 
+            from "Regimes" rg 
+            where 
+                rg."nomRegime"='Sans gluten'
+                and 
+                rg."idRegime" 
+                in (
+                    select ru."idRegime" 
+                    from "Regimes_Utilisateurs" ru  
+                    where ru."idUt" 
+                    in (
+                        select pru."idUt" 
+                        from "Planning_Recettes_Utilisateurs" pru )))));
+
+
+
+-- SELECT "idRecette", "nomRecette"
+--   FROM "Recettes" r
+--   WHERE ("calories" / "nombreDePersonnes") < 200
+--     AND r."idRecette" NOT IN (
+--       SELECT "idRecette"
+--         FROM "Recettes"
+--         WHERE "idRecette" IN (
+--           SELECT "idRecette"
+--             FROM "Ingredients_Recettes"
+--             WHERE "idIngredient" IN (
+--               SELECT iir."idIngredient"
+--                 FROM "Regimes" r, "Ingredients_Incompatibles_Regimes" iir
+--                 WHERE r."idRecette" = iir."idRegime" AND r."nomRegime" = 'Sans gluten'
+--             )
+--         )
+--     )
+--     AND r."idRecette" IN (
+--       SELECT DISTINCT "idRecette"
+--       FROM "Planning_Recettes_Utilisateurs"
+--       WHERE "idRecette" = r."idRecette"
+--     );
--- requetes/sql/02.sql
+++ requetes/sql/02.sql
@@ -1,15 +1,11 @@
--- La recette la plus souvent présente dans des plannings d’utilisateurs.
-select r."nomRecette"
-from "Recettes" r
-where "idRecette" in (
-    select pl."idRecette"
-    from "Planning_Recettes_Utilisateurs" pl
-    having count(pl."idRecette") = (
-        select max(count(pl."idRecette"))
-        from "Planning_Recettes_Utilisateurs"
-        group by pl."idRecette"
-    )
-    group by r."idRecette"
-);
-
-select 'delete on table' || table_name || 'cascade constraints;' from user_tables;
+select "nomRecette"
+    from "Recettes"
+     WHERE "idRecette" in
+           (select "idRecette"
+                from "Planning_Recettes_Utilisateurs"
+                    having count("idRecette")=(
+                        select max(count("idRecette"))
+                            from "Planning_Recettes_Utilisateurs"
+                               group by "idRecette"
+            )
+            group by "idRecette");
